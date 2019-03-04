5c2458ceab875391a3c2137ac2cb542a8b3e14ca
pohor <ola.pohorecka@gmail.com>
2019-03-04 13:44:01 +0100

    Add controller actions to create subscriptions and customers once a succes response from Fakepay is returned

--- app/controllers/subscription_requests_controller.rb
+++ app/controllers/subscription_requests_controller.rb
@@ -9,7 +9,15 @@ class SubscriptionRequestsController < ApplicationController
     @subscription_request = SubscriptionRequest.new(subscription_request_params)
     if @subscription_request.valid?
       puts "valid"
-      @customer = Customer.create(name: @subscription_request.name)
+      @response = @subscription_request.send_request
+        if @response["success"] == true
+          @customer = Customer.create(customer_params)
+          @subscription = Subscription.create(subscription_params)
+        elsif @response["success"] == false
+          puts "error" #handle code errors here
+        else
+          puts "another error" #handle incorrect API key here
+        end
     else
       render :json => { :errors => @subscription_request.errors }
     end
@@ -19,6 +27,14 @@ class SubscriptionRequestsController < ApplicationController
 
   def subscription_request_params
     params.require(:subscription_request).permit(:name, :address, :zipcode, :card_num, :expiration, :cvv, :billing_zip, :plan_id)
+  end
+
+  def customer_params
+    customer_params = { name: @subscription_request.name }
+  end
+
+  def subscription_params
+     subscription_params = { customer_id: @customer.id, plan_id: @subscription_request.plan_id, token: @response["token"], price: Plan.find_by(id: @subscription_request.plan_id).price }
   end
 
 end
